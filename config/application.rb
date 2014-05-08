afe1522e0e63d19d23148d8a7f2b4f84a9bd7e87
Lloyd D'Silva <lloyddsilva@gmail.com>
2014-05-08 15:32:18 +0800

    heroku config

--- config/application.rb
+++ config/application.rb
@@ -25,5 +25,8 @@ module LocalboardV1
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    config.i18n.enforce_available_locales = false
+
+    config.assets.paths << Rails.root.join("app", "assets", "fonts")
   end
 end
